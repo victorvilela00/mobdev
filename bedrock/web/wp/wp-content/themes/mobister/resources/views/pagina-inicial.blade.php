8265b3bfc3673c17380acd2f5df0652d8709a2f6
João Victor <victorvilela00@gmail.com>
2019-03-03 22:52:41 -0300

    Carousel finalizado

--- bedrock/web/wp/wp-content/themes/mobister/resources/views/pagina-inicial.blade.php
+++ bedrock/web/wp/wp-content/themes/mobister/resources/views/pagina-inicial.blade.php
@@ -9,7 +9,7 @@
 <div class="secao secao1">
   <div class="container">
     <div class="row">
-        <div class="col-md-7">
+        <div class="col-md-6">
           <h2><?php the_field('titulo_secao1') ?></h2>
           <p><?php the_field('texto_secao1') ?></p>
           <div class="center" style="margin-top: 60px">
@@ -39,18 +39,47 @@
 <div class="secao secao2">
   <div class="container">
     <div class="row">
-        <div class="col-md-7">
-          <h2><?php the_field('titulo_secao2') ?></h2>
-          <p><?php the_field('texto_secao2') ?></p>
+      <div class="col-md-6">
+        <h2><?php the_field('titulo_secao2') ?></h2>
+        <p><?php the_field('texto_secao2') ?></p>
+      </div>
+    </div>
+        <!-- carousel -->
+    
+    <div class="row">
+        <div class="col-md-6">
+          <div id="carousel-indicator" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-indicator" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-indicator" data-slide-to="1"></li>
+              <li data-target="#carousel-indicator" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/slide1.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/slide2.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/slide3.png" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel-indicator" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-indicator" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Próximo</span>
+            </a>
+          </div>
+        </div>
 
-        </div>
-        <div class="col-md-4 offset-md-6 form-box">
-            testando...
-        </div>
+
     </div>
   </div>
 </div>
-
 
 
 @endsection
